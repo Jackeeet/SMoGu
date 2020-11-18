ea4c16c8f4934f31fff9e89fe38616e35346469c
roman-sabur0v <roma.saburov.01@mail.ru>
2020-11-18 23:07:15 +0400

    был создан enum для отрезков времени отображения графика

---
+++ SMoGu.App/TimeOptions.cs
@@ -0,0 +1,12 @@
+﻿namespace SMoGu.App
+{
+    public enum TimeOptions
+    {
+        One_Day,
+        One_Week,
+        One_Month,
+        Three_Months,
+        Half_Year,
+        One_Year
+    }
+}
