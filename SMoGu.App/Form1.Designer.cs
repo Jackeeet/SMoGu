7df2496a371cd6338441fad077611d5bfd831e9f
Polina Grabar <polina.grabar@gmail.com>
2020-11-24 22:17:20 +0400

    Added new files to make a complete project & fixed namespace issues

--- SMoGu.App/Form1.Designer.cs
+++ SMoGu.App/Form1.Designer.cs
@@ -1,17 +1,16 @@
-﻿
-namespace SMoGu.App
+﻿namespace SMoGu.App
 {
     partial class Form1
     {
         /// <summary>
-        /// Required designer variable.
+        /// Обязательная переменная конструктора.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
         /// <summary>
-        /// Clean up any resources being used.
+        /// Освободить все используемые ресурсы.
         /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
@@ -21,21 +20,234 @@ namespace SMoGu.App
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Код, автоматически созданный конструктором форм Windows
 
         /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title1 = new System.Windows.Forms.DataVisualization.Charting.Title();
+            this.solidGauge1 = new LiveCharts.WinForms.SolidGauge();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // solidGauge1
+            // 
+            this.solidGauge1.Location = new System.Drawing.Point(752, 430);
+            this.solidGauge1.Name = "solidGauge1";
+            this.solidGauge1.Size = new System.Drawing.Size(200, 100);
+            this.solidGauge1.TabIndex = 1;
+            this.solidGauge1.Text = "solidGauge1";
+            // 
+            // chart1
+            // 
+            this.chart1.BorderSkin.BorderWidth = 0;
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            this.chart1.Location = new System.Drawing.Point(30, 81);
+            this.chart1.Name = "chart1";
+            series1.BorderWidth = 3;
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series1.Color = System.Drawing.Color.Lime;
+            series1.Name = "Series1";
+            series1.ShadowColor = System.Drawing.Color.Lime;
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(513, 369);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            title1.BorderWidth = 3;
+            title1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            title1.Name = "Title1";
+            title1.Text = "Grafic value";
+            this.chart1.Titles.Add(title1);
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(336, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Create grafic ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonCreateGrafic);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(192, 32);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 29);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(61, 12);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(76, 17);
+            this.radioButton1.TabIndex = 5;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "USD/RUB";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(61, 35);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(76, 17);
+            this.radioButton2.TabIndex = 6;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "EUR/RUB";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(61, 58);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(75, 17);
+            this.radioButton3.TabIndex = 7;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "CNY/RUB";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(572, 32);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(158, 29);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Create investment ";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button4.Location = new System.Drawing.Point(572, 76);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(158, 29);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Track investment";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(128, 3);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(44, 17);
+            this.radioButton7.TabIndex = 13;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "Day";
+            this.radioButton7.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.radioButton7.UseVisualStyleBackColor = true;
+            this.radioButton7.CheckedChanged += new System.EventHandler(this.radioButton7_CheckedChanged);
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(67, 3);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(55, 17);
+            this.radioButton6.TabIndex = 12;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "Month";
+            this.radioButton6.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.radioButton6.UseVisualStyleBackColor = true;
+            this.radioButton6.CheckedChanged += new System.EventHandler(this.radioButton6_CheckedChanged);
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(14, 3);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(47, 17);
+            this.radioButton5.TabIndex = 11;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "Year";
+            this.radioButton5.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.radioButton5.UseVisualStyleBackColor = true;
+            this.radioButton5.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton6);
+            this.panel1.Controls.Add(this.radioButton5);
+            this.panel1.Controls.Add(this.radioButton7);
+            this.panel1.Location = new System.Drawing.Point(89, 456);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(327, 30);
+            this.panel1.TabIndex = 15;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(800, 515);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.solidGauge1);
+            this.Name = "Form1";
+            this.Text = " ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+        private LiveCharts.WinForms.SolidGauge solidGauge1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.Panel panel1;
     }
 }
 
--- SMoGu.App/Form1.cs
+++ SMoGu.App/Form1.cs
@@ -7,14 +7,155 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace SMoGu.App
 {
     public partial class Form1 : Form
     {
+        Tuple<int, string> valueX = new Tuple<int, string>(0, "");
+        Tuple<int, string> valueY = new Tuple<int, string>(0, "");
+        //для valueY.Item1 можно сделать очередь, то есть при нажатии на кнопку выбора валюты будет
+        //браться опредленная очередь
+
         public Form1()
         {
             InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonCreateGrafic(object sender, EventArgs e)
+        {
+            var queueItems = new Queue<double>(); // очередь данных отношения валют
+            queueItems.Enqueue(1);    //проба
+            queueItems.Enqueue(3);    //проба
+            queueItems.Enqueue(15);   //проба
+            queueItems.Enqueue(5);    //проба
+            queueItems.Enqueue(3);    //проба*/
+
+            if (valueX.Item1 == 0) throw new ArgumentException();
+            if (this.radioButton5.Checked)
+            {
+                Axis ax = new Axis();
+                ax.Title = valueX.Item2;
+                this.chart1.ChartAreas[0].AxisX = ax;
+            }
+            else if (this.radioButton6.Checked)
+            {
+                Axis ax = new Axis();
+                ax.Title = valueX.Item2;
+                this.chart1.ChartAreas[0].AxisX = ax;
+            }
+            else if (this.radioButton7.Checked)
+            {
+                Axis ax = new Axis();
+                ax.Title = valueX.Item2;
+                this.chart1.ChartAreas[0].AxisX = ax;
+            }
+
+            if (this.radioButton1.Checked)
+            {
+                Axis ax = new Axis();
+                ax.Title = valueY.Item2;
+                this.chart1.ChartAreas[0].AxisY = ax;
+            }
+            else if (this.radioButton2.Checked)
+            {
+                Axis ax = new Axis();
+                ax.Title = valueY.Item2;
+                this.chart1.ChartAreas[0].AxisY = ax;
+            }
+            else if (this.radioButton3.Checked)
+            {
+                Axis ax = new Axis();
+                ax.Title = valueY.Item2;
+                this.chart1.ChartAreas[0].AxisY = ax;
+            }
+            double left = 0, x, y;
+            this.chart1.Series[0].Points.Clear();
+            x = left;
+            /*while (x < count)//count это размер графика, который бует выбираться пользователем
+            {
+                try
+                {
+                    y = queueItems.Dequeue();
+                }
+                catch (InvalidOperationException)
+                {
+                    break;
+                }
+
+                this.chart1.Series[0].Points.AddXY(x, y);
+                x += valueX.Item1;
+            }*/
+            while (true)
+            {
+                try
+                {
+                    y = queueItems.Dequeue();
+                }
+                catch (InvalidOperationException)
+                {
+                    break;
+                }
+
+                this.chart1.Series[0].Points.AddXY(x, y);
+                x += valueX.Item1;
+            }
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            valueY = new Tuple<int, string>(0, "USD");
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            valueY = new Tuple<int, string>(0, "EUR");
+        }
+           
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+            valueY = new Tuple<int, string>(0, "CNY");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void splitContainer1_Panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
+        {
+            valueX = new Tuple<int, string>(1, "Year");
+        }
+
+        private void radioButton6_CheckedChanged(object sender, EventArgs e)
+        {
+            valueX = new Tuple<int, string>(1, "Month");
+        }
+
+        private void radioButton7_CheckedChanged(object sender, EventArgs e)
+        {
+            valueX = new Tuple<int, string>(1, "Day");
         }
     }
 }
