9e37834170c7136b627ecccfb7654cc80c7d87f8
roman-sabur0v <roma.saburov.01@mail.ru>
2020-12-18 12:06:36 +0400

    добавил тестики к очереди и парсера

---
+++ SMoGu.App.Test/DataParserTests.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMoGu.App.Test
+{
+
+    [TestClass]
+    public class DataParserTests
+    {
+        [TestMethod]
+        //проверка на правильность работы парсера данных с сайта ЦБ РФ
+        public void CorrectWorkingDataParser()
+        {
+            var data = new Queue<Tuple<decimal, decimal, decimal, DateTime>>();
+            data.Enqueue(Tuple.Create((decimal)73.6618, (decimal)89.2044, (decimal)11.2752, new DateTime(2020, 12, 9, 1, 0, 0)));
+            data.Enqueue(Tuple.Create((decimal)73.3057, (decimal)88.9418, (decimal)11.2126, new DateTime(2020, 12, 10, 1, 0, 0)));
+            data.Enqueue(Tuple.Create((decimal)73.7124, (decimal)89.1330, (decimal)11.2578, new DateTime(2020, 12, 11, 1, 0, 0)));
+            data.Enqueue(Tuple.Create((decimal)73.1195, (decimal)88.7744, (decimal)11.1778, new DateTime(2020, 12, 12, 1, 0, 0)));
+            data.Enqueue(Tuple.Create((decimal)72.9272, (decimal)88.5847, (decimal)11.1557, new DateTime(2020, 12, 15, 1, 0, 0)));
+            data.Enqueue(Tuple.Create((decimal)73.4453, (decimal)89.1846, (decimal)11.2219, new DateTime(2020, 12, 16, 1, 0, 0)));
+            data.Enqueue(Tuple.Create((decimal)73.4201, (decimal)89.3229, (decimal)11.2310, new DateTime(2020, 12, 17, 1, 0, 0)));
+            data.Enqueue(Tuple.Create((decimal)72.9781, (decimal)89.2887, (decimal)11.1693, new DateTime(2020, 12, 18, 1, 0, 0)));
+
+            var parser = new DailyDataParser(TimeOptions.One_Week);
+            var testData = parser.GetData();
+            for (int i=0; i<testData.Count; i++)
+            {
+                Assert.AreEqual(data.Dequeue(), testData.Dequeue());
+            }
+        }
+
+    }
+}
---
+++ SMoGu.App.Test/QueueTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMoGu.App.Test
+{
+
+    [TestClass]
+    public class QueueTests
+    {
+        [TestMethod]
+        //проверка на правильность заполнения очереди
+        public void CorrectFillingQueue()
+        {
+            var queue = new Queue<int>();
+            for (int i = 0; i < 10; i++)
+                queue.Enqueue(i);
+            for (int i = 0; i < 10; i++)
+                Assert.AreEqual(i, queue.Dequeue());
+        }
+
+        [TestMethod]
+        //проверка правильности установки метки на первый элемент
+        public void CorrectPointerToTheFirstElement()
+        {
+            var queue = new Queue<string>();
+            queue.Enqueue("aa");
+            queue.Enqueue("bb");
+            queue.Enqueue("xx");
+            Assert.AreEqual("aa", queue.Head.Value);
+        }
+
+        [TestMethod]
+        //проверка правильности установки метки на последний элемент
+        public void CorrectPointerToTheLastElement()
+        {
+            var queue = new Queue<double>();
+            queue.Enqueue(22.4);
+            queue.Enqueue(34.65);
+            queue.Enqueue(7.998);
+            Assert.AreEqual(7.998, queue.Tail.Value);
+        }
+    }
+}
