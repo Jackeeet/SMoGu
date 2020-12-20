3888bd958253ef39051a837bb4ce3035ec7fb3da
Polina Grabar <polina.grabar@gmail.com>
2020-12-20 22:05:28 +0400

    новый тест для калькулятора

---
+++ SMoGu.App.Test/PredictionCalcTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace SMoGu.App.Test
+{
+    [TestClass]
+    public class PredictionCalcTests
+    {
+        [TestMethod]
+        public void CalcDeterminesCoefs()
+        {
+            var data = new List<Tuple<decimal, DateTime>>
+            {
+                Tuple.Create(76.7600m, DateTime.Now.AddDays(-3)),
+                Tuple.Create(75.8146m, DateTime.Now.AddDays(-2)),
+                Tuple.Create(75.4727m, DateTime.Now.AddDays(-1)),
+                Tuple.Create(75.4518m, DateTime.Now)
+            };
+
+            MethodInfo modifier = typeof(PredictionCalculator).GetMethod("DetermineARCoefs", BindingFlags.Static | BindingFlags.NonPublic);
+            var coefs = (decimal[])modifier.Invoke(null, new object[] { data, 2 });
+            var expected = new[] { 1.535m, -0.533m };
+            Assert.AreEqual((double)expected[0], (double)coefs[0], 10e-4);
+            Assert.AreEqual((double)expected[1], (double)coefs[1], 10e-4);
+        }
+    }
+}
